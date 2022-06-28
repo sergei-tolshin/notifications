abc07c5aa3c0e85af2cf8595dc9928d847b87f1a
Sergei Tolshin <tolshin.sergei@yandex.ru>
2022-06-28 21:03:18 +0500

    Создан API для приема событий

--- db_scheme/notice_database.sql
+++
@@ -1,8 +0,0 @@
--- Расширения для генерации UUID
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- Cхема 'notice' 
-CREATE SCHEMA IF NOT EXISTS notice;
-
--- Таблица 'Шаблоны уведомлений'
-
--- Таблица 'История отправки уведомлений'
--- worker/main.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-
-import config
-
-
-app = Celery(__name__, include=['tasks'])
-app.config_from_object(config)
